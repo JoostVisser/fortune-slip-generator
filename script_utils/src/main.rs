c7403d5aa72feef1ed3cec95765127889afc356c
Joost Visser <Joost.Visser@smartqare.nl>
2023-06-18 23:20:44 +0200

    ci(feat): add badge call

---
+++ script_utils/src/main.rs
@@ -0,0 +1,15 @@
+use std::fs::File;
+
+use serde_json::Value;
+
+
+fn main() {
+    let jscpd_file = File::open("report/jscpd-report.json").unwrap();
+    let jscpd_report: Value = serde_json::from_reader(jscpd_file).unwrap();
+    let dupl_percentage = jscpd_report["statistics"]["total"]["percentage"]
+        .as_f64()
+        .unwrap();
+
+    print!(r#"DUPL={};DUPL_PERCENT="{:.2}%""#, dupl_percentage, dupl_percentage);
+
+}
