d155ce30e53e01cd3ccb5237d6f474c0516842af
Joost Visser <Joost.Visser@smartqare.nl>
2023-06-18 23:24:06 +0200

    refactor: update formatting

--- script_utils/src/main.rs
+++ script_utils/src/main.rs
@@ -2,7 +2,6 @@ use std::fs::File;
 
 use serde_json::Value;
 
-
 fn main() {
     let jscpd_file = File::open("report/jscpd-report.json").unwrap();
     let jscpd_report: Value = serde_json::from_reader(jscpd_file).unwrap();
@@ -10,6 +9,8 @@ fn main() {
         .as_f64()
         .unwrap();
 
-    print!(r#"DUPL={};DUPL_PERCENT="{:.2}%""#, dupl_percentage, dupl_percentage);
-
+    print!(
+        r#"DUPL={};DUPL_PERCENT="{:.2}%""#,
+        dupl_percentage, dupl_percentage
+    );
 }
