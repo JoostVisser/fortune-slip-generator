58598f502fc13fad67e2f0d3f5b8a14358504453
Joost Visser <Joost.Visser@smartqare.nl>
2023-07-08 12:10:53 +0200

    style: fix clippy issues

--- src/cli/windows.rs
+++ src/cli/windows.rs
@@ -1,6 +1,8 @@
+use std::process::Command;
+
 pub fn press_a_key_to_continue_windows_only() {
     if cfg!(target_os = "windows") {
-        println!("");
+        println!();
         println!("Press any key to continue...");
         let _ = Command::new("cmd.exe").arg("/c").arg("pause").status();
     }
